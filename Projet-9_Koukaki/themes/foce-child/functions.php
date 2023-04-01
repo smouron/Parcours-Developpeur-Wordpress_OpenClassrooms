46c916cbb1c1dd206b713978a18dfe75833ef738
Stephane Mouron <stephane.mouron@free.fr>
2023-04-01 10:06:30 +0200

    :Projet 9 : activation du thème enfant et mise en place des premières animations avec CSS et JavaScript

---
+++ Projet-9_Koukaki/themes/foce-child/functions.php
@@ -0,0 +1,23 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    //  Chargement du style personnalisé pour le theme
+    wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css' );
+
+    // Enqueue Custom Scripts
+    wp_enqueue_script( 'order-custom-scripts', get_theme_file_uri( '/assets/js/custom-scripts.js' ), array(), '1.0.0', true );
+
+}
+
+// Get customizer options form parent theme
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+        update_option( 'theme_mods_' . get_template(), $value );
+        return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
