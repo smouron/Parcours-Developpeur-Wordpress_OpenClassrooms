ce7721c1c51d9f5b1b7bcc5960ada254fb48a740
Stephane Mouron <stephane.mouron@free.fr>
2023-04-14 09:51:42 +0200

    Projet 9 : Mise en place de jQuery dans le theme enfant pour la modal et modification de la gestion du déplacement des nuages pour que cela soit plus fluide

--- Projet-9_Koukaki/themes/foce-child/assets/js/custom-scripts.js
+++ Projet-9_Koukaki/themes/foce-child/assets/js/custom-scripts.js
@@ -4,7 +4,7 @@
  * Released on: April 2023
  */
 
-// console.log("Démarrage du script !");
+console.log("Démarrage du script !");
 
 // Différer le lancement du script => ne se lance qu'une fois que tout le HTML a été chargé
 if (document.readyState === "complete") {
@@ -16,39 +16,67 @@ if (document.readyState === "complete") {
 }
 
 function monScript() {
-  // console.log("HTML prêt !");
+  console.log("HTML prêt !");
+
+  // Gestion de la fermeture et de l'ouverture de la modale avec jQuery
+  $(".modal-open").click(function () {
+    console.log("modal-trigger cliqué");
+    $(".modal__content").animate({ height: "toggle", opacity: "toggle" }, 1000);
+    $(".modal__content").toggleClass("open");
+    $(".modal__burger").toggleClass("close");
+  });
+  $("a").click(function () {
+    if ($(".modal__content").hasClass("open")) {
+      $(".modal__content").animate(
+        { height: "toggle", opacity: "toggle" },
+        1000
+      );
+      $(".modal__content").removeClass("open");
+      $(".modal__burger").removeClass("close");
+    }
+  });
+
+   // Initialize Swiper
+   var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 60,
+    speed: 1000,
+    autoplay: {
+        delay: 250,
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 3,
+    coverflowEffect: {
+        rotate: 60,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+    },
+    autoplay: {
+        delay: 2500,
+        // disableOnInteraction: false,
+    },
+
+});
 
   let posX = 0;
   let mouveCloud = false;
 
   const root = document.documentElement;
-  const burger = document.querySelector(".modal__burger");
-  const content = document.querySelector(".modal__content");
-  const modalTriggers = document.querySelectorAll(".modal-trigger");
   const place = document.querySelector("#place");
-  const littleCloud = document.querySelector(".place--little_cloud");
-  const bigCloud = document.querySelector(".place--big_cloud");
 
-  // Gestion de la fermeture et de l'ouverture de la modale
-  modalTriggers.forEach((trigger) => {
-    trigger.addEventListener("click", () => {
-      toggleModal();
-    });
-  });
-
-  function toggleModal() {
-    burger.classList.toggle("close");
-    content.classList.toggle("hidden");
-    content.classList.toggle("visible");
-  }
-
-  // Gestion du déclenchement des évenements quand ils apparaissent
   const handleIntersect = (entries) => {
     entries.forEach(function (entry) {
       // Contrôle si l'élément à observer
       // est dans le ratio de la zone qui est affichée
       if (entry.intersectionRatio > ratio) {
         elementName = entry.target.className;
+        // console.log(elementName + " est visible");
+        
+        // Si on trouve l'élément indiqué, on active l'animation d'apparition
         if (
           elementName === "story hidden" ||
           elementName === "studio hidden" ||
@@ -72,17 +100,7 @@ function monScript() {
           entry.target.classList.add("animTitle");
           observer.unobserve(entry.target);
           entry.target.classList.remove("hidden");
-        }
-
-        // Si on a trouvé un des nuages on active l'autorisation de déplacement
-        if (
-          elementName === "place--big_cloud" ||
-          elementName === "place--little_cloud"
-        ) {
-          mouveCloud = true;
-        } else {
-          mouveCloud = false;
-        }
+        }        
       }
     });
   };
@@ -111,8 +129,6 @@ function monScript() {
   observer.observe(document.querySelector(".studio__title"));
   observer.observe(document.querySelector(".characters__title"));
   observer.observe(document.querySelector(".place__title"));
-  observer.observe(bigCloud);
-  observer.observe(littleCloud);
 
   // Contrôle si on scroll sur la fenêtre
   window.addEventListener("scroll", () => {
@@ -128,14 +144,10 @@ function monScript() {
       }
     }, 500);
 
-    // on ne bouge les nuages que s'ils ont été détécté à l'affichage
-    if (mouveCloud) {
+    // On bouge les 2 nuages en fonction du scroll
       posX = Math.round(0 - (window.scrollY - place.offsetTop - 200));
       if (posX <= 0 && posX > -400) {
         root.style.setProperty("--posX", posX + "px");
-      }
-    } else {
-      posX = 0;
-    }
+      }     
   });
 }
